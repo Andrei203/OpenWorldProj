9928ab161b1170985a20d5bceec1abebedf8b696
Andrei <andrei2.barlea@live.uwe.ac.uk>
2022-02-21 15:50:58 +0000

    terrain generation on camera pos

---
+++ Assets/Scenes/Scripts/TerrainChunkSystem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainChunkSystem : MonoBehaviour
+{
+    public List<TerrainTiles> tiles;
+    //public float tileRadius = 0.6f;
+    public Camera mainCamera;
+    public float tileRange = 25.0F;
+
+    //private CullingGroup _cullingGroup;
+    //private BoundingSphere[] _boundingSpheres = new BoundingSphere[0];
+
+    private void OnEnable()
+    {
+        //InitCullingGroup();
+    }
+
+    private void OnDisable()
+    {
+        //_cullingGroup.Dispose();
+    }
+
+    /*private void InitCullingGroup()
+    {
+        _cullingGroup = new CullingGroup();
+        _boundingSpheres = new BoundingSphere[tiles.Count];
+
+        for (var i = 0; i < _boundingSpheres.Length; i++)
+        {
+            _boundingSpheres[i].position = tiles[i].transform.position;
+            _boundingSpheres[i].radius = tileRadius;
+        }
+        
+        _cullingGroup.SetBoundingSpheres(_boundingSpheres);
+        _cullingGroup.targetCamera = Camera.main;
+        
+        _cullingGroup.onStateChanged = OnStateChanged;
+    }
+    
+    private void OnStateChanged(CullingGroupEvent evt)
+    {
+        Debug.Log("GHBNADIGFJAHYUGTGTFTGF");
+        if (evt.hasBecomeVisible)
+            tiles[evt.index].InstantiateTileAsset();
+        
+        if(evt.hasBecomeInvisible)
+            tiles[evt.index].ReleaseTileAsset();
+    }*/
+
+    private void Update()
+    {
+        foreach (TerrainTiles tile in tiles)
+        {
+            bool inRange = Vector3.Distance(mainCamera.transform.position, tile.transform.position) < tileRange;
+            if (!tile.enabled && inRange)
+            {
+                tile.InstantiateTileAsset();
+            }
+            else if (tile.enabled && !inRange)
+            {
+                tile.ReleaseTileAsset();
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/TerrainTiles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class TerrainTiles : MonoBehaviour
+{   
+    public AssetReference assetReference;
+
+    private AsyncOperationHandle<GameObject> _operationHandle;
+    private GameObject _tileAsset;
+
+    private void Awake()
+    {
+        enabled = false;
+    }
+
+    public async Task InstantiateTileAsset()
+    {
+        enabled = true;
+        if (_tileAsset != null) return;
+        
+        _operationHandle = assetReference.InstantiateAsync(transform.position, transform.rotation, transform);
+
+        await _operationHandle.Task;
+
+        _tileAsset = _operationHandle.Result;
+    }
+
+    public void ReleaseTileAsset()
+    {
+        enabled = false;
+        if (_tileAsset == null) return;
+        
+        Addressables.ReleaseInstance(_operationHandle);
+        Addressables.ReleaseInstance(_tileAsset);
+    }
+}
