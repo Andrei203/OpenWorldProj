0e6408fa7c48791c05c9b044a15ef7d91bf9a16c
Andrei <andrei2.barlea@live.uwe.ac.uk>
2022-03-11 14:11:39 +0000

    score system, spinning objects, enemy bullets follow player

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+   [SerializeField] private Vector3 cubeRotate;
+   [SerializeField] private float rotateSpeed;
+
+    void Update()
+   {
+        transform.Rotate(cubeRotate * rotateSpeed * Time.deltaTime);  
+   }
+}
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int score;
+    public AudioSource collectScoreSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectScoreSound.Play();
+        score += 50;
+        scoreText.GetComponent<Text>().text = "SCORE: " + score;
+        Destroy(gameObject);
+    }
+}
